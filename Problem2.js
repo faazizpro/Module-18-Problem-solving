089dde6e9cbe344b479ef05e42c55037addc825e
faazizpro <fahadafs11@gmail.com>
2022-12-25 15:19:19 +0600

    Problem 2 has been solved

---
+++ Problem2.js
@@ -0,0 +1,7 @@
+let todos = ['1. Watch Module', '2. Make notes', '3. Practice module', '4. Think about the problem', '5. Join the support session'];
+let i = 0
+while(i < todos.length){
+    let todo = todos[i];
+    console.log(todo);
+    i++;
+}
