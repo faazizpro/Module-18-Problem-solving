e2297191da94745fb895ad0ea217cfec8e23452e
faazizpro <fahadafs11@gmail.com>
2022-12-25 15:38:16 +0600

    Problem 4 has been done

---
+++ Problem4.js
@@ -0,0 +1,6 @@
+let todos = ['1. Watch Module', '2. Make notes', '3. Practice module', '4. Think about the problem', '5. Join the support session'];
+
+for(let i = 4; i >= 0; i--){
+    let todo = todos[i];
+    console.log(todo);
+}
